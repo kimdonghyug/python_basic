8a35b99f6ee23e54ca75e0c0d555008a72bf63f0
kimdonghyug <kdh20060907@gmail.com>
2022-10-29 12:20:37 +0900

    수정

--- dqy1.py
+++ dqy1.py
@@ -1,9 +1,34 @@
+input_num = int(input("Enter the number of rows: "))
+list = [] #an empty list
 num = 0
-hap = 0
-while num < 10:
-    num +=1
-    if num %3 == 0:
-        continue
-        
-    hap += num 
-print(f"1~100까지의 수중에서 3의 배수가 아닌 수들의 합은 {hap}")
+for n in range(input_num):
+    list.append([])
+    list[n].append(1)
+
+    for m in range(1, n):
+        list[n].append(list[n - 1][m - 1] + list[n - 1][m])
+
+    list[n].append(1)
+del list[0][0]
+
+pascal = []
+
+for i in range(input_num):
+    temp = ''
+    for j in range(i+1):
+        temp += (str(list[i][j]) + ' ')
+    
+    pascal.append(temp)
+
+last_len = len(pascal[-1])
+
+last_len1 = len(pascal[-1])
+
+for i in range(input_num):
+    while last_len1 > 0:
+        last_len_half = last_len / 2
+        vacuum = last_len_half * ''
+        pascal[num].append(vacuum)
+        num +=1
+        last_len1 -=1
+print(pascal)
